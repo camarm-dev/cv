00457cfbe1559b301cf6e4e40f2088a3fd69af6f
camarm-dev <contact@camarm.dev>
2024-03-30 12:35:01 +0100

    fix: better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 docker context use production
 
-docker build -f Dockerfile -t cv .
+echo "Building container..."
+docker build -f Dockerfile -t cv . > /dev/null
 
-docker stop cv
-docker rm cv
+echo "Deploying container..."
+docker stop cv.camarm.fr
+docker rm cv.camarm.fr
 
 docker run -d --name cv.camarm.fr -p 3001:3000 --restart unless-stopped cv
 
+echo "Deployed. Opening cv.camarm.fr."
 open cv.camarm.fr
